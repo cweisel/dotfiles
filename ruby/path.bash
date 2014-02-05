2372874e7d65fcac2d7afd199803fafe1a14ea16
Chris Weisel <cweisel@onshift.com>
2014-02-05 23:18:57 +0000

    Removing the ruby stuff, because I don't need it
    At least I don't think so.

--- ruby/path.bash
+++
@@ -1,2 +0,0 @@
-# add rbenv's bin and shims directories to ${PATH}.
-export PATH=${PATH}:${HOME}/.rbenv/bin:${HOME}/.rbenv/shims
--- ruby/rbenv.bash
+++
@@ -1,2 +0,0 @@
-# load rbenv into the shell.
-eval "$(rbenv init -)"
